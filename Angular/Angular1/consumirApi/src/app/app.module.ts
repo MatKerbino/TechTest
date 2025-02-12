e1ca2434ae387eba8d7e0d282a2621f1070cd277
Matheus Willian <159083906+MatKerbino@users.noreply.github.com>
2025-02-12 19:05:42 -0300

    Angular Config

---
+++ Angular/Angular1/consumirApi/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppComponent
+    ],
+})
+export class AppModule{
+    
+}
