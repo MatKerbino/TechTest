525409efe265c14f1c897dbefcbf3e95ec28dc4d
Matheus Willian <159083906+MatKerbino@users.noreply.github.com>
2025-02-13 19:11:02 -0300

    new

--- Angular/Angular1/consumirApi/src/app/services/music.service.ts
+++ Angular/Angular1/consumirApi/src/app/services/music.service.ts
@@ -1,10 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MusicService {
+
+  private url  = environment;
 
   constructor(httpClient: HttpClient) { 
   }
